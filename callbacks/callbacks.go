f24e5b8750a75b95648fb0a75d66c140c3d2c133
weibaohui <weibaohui@yeah.net>
2024-10-22 13:47:38 +0800

    refactor(callbacks): 更新回调函数注册的事件名称
    - 将事件名称前缀从 "k8sorm" 改为 "kom"，统一命名风格
    - 修改了 get、list、create、update、patch 和 delete 回调函数的事件名称

--- callbacks/callbacks.go
+++ callbacks/callbacks.go
@@ -7,20 +7,20 @@ import (
 func RegisterDefaultCallbacks() {
 
 	queryCallback := kom.Init().Callback().Get()
-	_ = queryCallback.Register("k8sorm:get", Get)
+	_ = queryCallback.Register("kom:get", Get)
 
 	listCallback := kom.Init().Callback().List()
-	_ = listCallback.Register("k8sorm:list", List)
+	_ = listCallback.Register("kom:list", List)
 
 	createCallback := kom.Init().Callback().Create()
-	_ = createCallback.Register("k8sorm:create", Create)
+	_ = createCallback.Register("kom:create", Create)
 
 	updateCallback := kom.Init().Callback().Update()
-	_ = updateCallback.Register("k8sorm:update", Update)
+	_ = updateCallback.Register("kom:update", Update)
 
 	patchCallback := kom.Init().Callback().Patch()
-	_ = patchCallback.Register("k8sorm:patch", Patch)
+	_ = patchCallback.Register("kom:patch", Patch)
 
 	deleteCallback := kom.Init().Callback().Delete()
-	_ = deleteCallback.Register("k8sorm:delete", Delete)
+	_ = deleteCallback.Register("kom:delete", Delete)
 }
