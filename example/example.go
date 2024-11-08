1b5204053b45ff7de852a8abf326cebb807914b5
weibaohui <weibaohui@yeah.net>
2024-11-08 17:46:52 +0800

    test(example): 上传大文件测试
    - 新增 dd 函数创建指定大小的文件
    - 在主函数中调用 dd 函数生成 100MB 测试文件
    - 使用 UploadFile 方法上传测试文件到集群
    - 增加日志记录和错误处理

--- example/example.go
+++ example/example.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
+	"os"
 	"time"
 
 	"github.com/weibaohui/kom/kom"
@@ -587,11 +589,17 @@ spec:
 		klog.Errorf("Error executing command: %v", err)
 	}
 	fmt.Printf("从/etc/xyz读取到%s\n", string(file))
-	//
-	// err = kom.DefaultCluster().Namespace("default").
-	// 	Name("random-char-pod").
-	// 	ContainerName("container").Poder().
-	// 	UploadFile("/etc/xyz", context)
+
+	dd("test", 100*1024*1024)
+	op, err := os.Open("test")
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+	defer op.Close()
+	err = kom.DefaultCluster().Namespace("kindplus").
+		Name("kindplus-66bd8b4d5f-j99f6").
+		ContainerName("kindplus").Poder().
+		UploadFile("/etc/", op)
 }
 
 func multiCluster() {
@@ -669,4 +677,36 @@ func fetchDoc2() {
 
 	// json := utils.ToJSON(tree)
 	fmt.Printf("[%s/%s:%s]%s\n", group, version, kind, tree.ID)
+}
+
+// 创建一个指定大小的文件。
+// 10 * 1024 * 1024 =10MB
+func dd(filePath string, size int64) error {
+	// 指定文件路径和大小（以字节为单位）
+	fileSize := size // 10MB
+
+	// 创建文件
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return err
+	}
+	defer file.Close()
+
+	// 定义写入的数据块，通常是一个固定的字节，重复写入直到达到指定大小
+	buffer := make([]byte, 1024) // 1KB的缓冲区，可以选择其他大小
+
+	// 写入数据直到文件达到指定大小
+	var written int64
+	for written < fileSize {
+		n, err := file.Write(buffer)
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+			return err
+		}
+		written += int64(n)
+	}
+
+	fmt.Printf("Successfully created a file of size %d bytes: %s\n", fileSize, filePath)
+	return nil
 }
