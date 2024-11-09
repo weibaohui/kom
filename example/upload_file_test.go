3511076e905add44c5b2f1efd2a72a882a5f3fa7
weibaohui <weibaohui@yeah.net>
2024-11-09 15:56:45 +0800

    test(example): 重构上传文件测试并添加保存文件测试
    -重构了上传文件测试，简化了代码结构
    - 添加了保存文件和下载文件的测试用例- 改进了错误处理，使用 t.Fatalf 替代 fmt.Printf 和 log.Fatalf
    - 优化了测试输出，使其更加清晰

--- example/upload_file_test.go
+++ example/upload_file_test.go
@@ -2,70 +2,23 @@ package example
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
-	"time"
 
 	"github.com/weibaohui/kom/kom"
 	"github.com/weibaohui/kom/utils"
 	"k8s.io/klog/v2"
 )
 
-// TestMain 是测试的入口函数
-
 func TestUploadFile(t *testing.T) {
-	yaml := `apiVersion: v1
-kind: Pod
-metadata:
-  name: random-char-pod
-  namespace: default
-spec:
-  containers:
-  - args:
-    - |
-      mkdir -p /var/log;
-      while true; do
-        random_char="A$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 1)";
-        echo $random_char | tee -a /var/log/random_a.log;
-        sleep 5;
-      done
-    command:
-    - /bin/sh
-    - -c
-    image: alpine
-    name: container
-`
-	result := kom.DefaultCluster().Applier().Apply(yaml)
-	for _, str := range result {
-		fmt.Println(str)
-	}
-	time.Sleep(time.Second * 20)
-
-	context := utils.RandNLengthString(20)
-	fmt.Printf("将%s写入/etc/xyz\n", context)
-	err := kom.DefaultCluster().Namespace("default").
-		Name("random-char-pod").
-		ContainerName("container").Poder().
-		SaveFile("/etc/xyz", context)
-	if err != nil {
-		klog.Errorf("Error executing command: %v", err)
-	}
-
-	file, err := kom.DefaultCluster().Namespace("default").
-		Name("random-char-pod").
-		ContainerName("container").Poder().DownloadFile("/etc/xyz")
-	if err != nil {
-		klog.Errorf("Error executing command: %v", err)
-	}
-	fmt.Printf("从/etc/xyz读取到%s\n", string(file))
 
 	// 模拟上传一个100M的临时文件
 	// 创建临时目录
 	tempDir, err := os.MkdirTemp("", "upload-*")
 	if err != nil {
-		fmt.Printf("error creating temp directory: %v", err)
+		t.Fatalf("error creating temp directory: %v", err)
 	}
 
 	// 使用原始文件名生成临时文件路径
@@ -73,30 +26,56 @@ spec:
 
 	err = dd(tempFilePath, 100*1024*1024)
 	if err != nil {
-		fmt.Printf("error creating temp test file %s: %v", tempFilePath, err)
+		t.Fatalf("error creating temp test file %s: %v", tempFilePath, err)
 	}
 	op, err := os.Open(tempFilePath)
 	if err != nil {
-		log.Fatalf("Failed to open file: %v", err)
+		t.Fatalf("Failed to open file: %v", err)
 	}
 	defer op.Close()
 	err = kom.DefaultCluster().Namespace("default").
-		Name("random-char-pod").
-		ContainerName("container").Poder().
+		Name("random").
+		ContainerName("random").Poder().
 		UploadFile("/etc/", op)
 
 	// 执行ls -lh /etc/ | grep test 查看文件是否已上传
 	var execResult []byte
 	err = kom.DefaultCluster().Namespace("default").
-		Name("random-char-pod").
-		ContainerName("container").
-		Command("ls", "-l /etc/ | grep test").
+		Name("random").
+		ContainerName("random").
+		Command("sh", "-c", "ls -lh /etc/ | grep test").
 		Execute(&execResult).Error
+	if err != nil {
+		t.Fatalf("Error executing command: %v", err)
+	}
+	fmt.Printf("ls test file info :\n%s", execResult)
+	if !strings.Contains(string(execResult), "100.0M") {
+		t.Fatalf("未找到上传文件，测试失败")
+	}
+}
+func TestSaveFile(t *testing.T) {
+
+	context := utils.RandNLengthString(20)
+	fmt.Printf("将%s写入/etc/xyz\n", context)
+	err := kom.DefaultCluster().Namespace("default").
+		Name("random").
+		ContainerName("random").Poder().
+		SaveFile("/etc/xyz", context)
 	if err != nil {
 		klog.Errorf("Error executing command: %v", err)
 	}
-	fmt.Printf("ls test file info :\n%s", execResult)
 
+	result, err := kom.DefaultCluster().Namespace("default").
+		Name("random").
+		ContainerName("random").Poder().DownloadFile("/etc/xyz")
+	if err != nil {
+		klog.Errorf("Error executing command: %v", err)
+	}
+	fmt.Printf("从/etc/xyz读取到%s\n", string(result))
+
+	if !strings.Contains(string(result), context) {
+		t.Fatalf("读取文件失败，应为%s,实际%s", context, string(result))
+	}
 }
 
 // 创建一个指定大小的文件。
