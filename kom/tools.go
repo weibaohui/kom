947db66ae456361d78417ff0f1655ff0fc2fc27a
weibaohui <weibaohui@yeah.net>
2024-11-09 19:34:44 +0800

    feat(tools): 添加通用对象转换工具
    添加了一个名为 Tools 的新工具包，提供了一个通用的转换函数 ConvertRuntimeObjectToTypedObject，用于将 runtime.Object 转换为指定的目标类型。这个工具包可以用于处理 Kubernetes API 中的非结构化数据，将其转换为具体的 Go 结构体类型。

---
+++ kom/tools.go
@@ -0,0 +1,32 @@
+package kom
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func Tools() *tools {
+	return &tools{}
+}
+
+type tools struct {
+}
+
+// ConvertRuntimeObjectToTypedObject 是一个通用的转换函数，将 runtime.Object 转换为指定的目标类型
+func (u *tools) ConvertRuntimeObjectToTypedObject(obj runtime.Object, target interface{}) error {
+	// 将 obj 断言为 *unstructured.Unstructured 类型
+	unstructuredObj, ok := obj.(*unstructured.Unstructured)
+	if !ok {
+		return fmt.Errorf("无法将对象转换为 *unstructured.Unstructured 类型")
+	}
+
+	// 使用 DefaultUnstructuredConverter 将 unstructured 数据转换为具体类型
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstructuredObj.Object, target)
+	if err != nil {
+		return fmt.Errorf("无法将对象转换为目标类型: %v", err)
+	}
+
+	return nil
+}
