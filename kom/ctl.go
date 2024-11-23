7f26bb00d31f1960226b0f1b85f0d6ee041670ae
weibaohui <weibaohui@yeah.net>
2024-11-23 22:10:44 +0800

    refactor(deploy): 重构 Deployment 的重启和扩缩容逻辑- 优化 Restart 方法，使用 patch 方式更新 annotations - 添加 Scale 方法，实现 Deployment 的扩缩容功能 - 改进错误日志，增加命名空间和名称信息 - 调整时间格式，使用 DateTime 替代 RFC3339

--- kom/ctl.go
+++ kom/ctl.go
@@ -1,9 +1,11 @@
 package kom
 
 import (
+	"fmt"
 	"time"
 
 	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog/v2"
 )
 
@@ -24,23 +26,50 @@ func (c *ctl) Deployment() *deploy {
 
 func (d *deploy) Restart() error {
 	var item v1.Deployment
+	err := d.kubectl.Get(&item).Error
+	if err != nil {
+		klog.Errorf("Deployment Restart %s/%s error :%v", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
+		d.kubectl.Error = err
+		return d.kubectl.Error
+	}
+	patchData := `{
+	"spec": {
+		"template": {
+			"metadata": {
+				"annotations": {
+						"kom.kubernetes.io/restartedAt": "%s"
+				}
+			}
+		}
+	}
+}`
+	patchData = fmt.Sprintf(patchData, time.Now().Format(time.DateTime))
+
+	err = d.kubectl.Resource(&item).
+		Patch(&item, types.MergePatchType, patchData).Error
+	if err != nil {
+		klog.Errorf("Deployment Restart %s/%s error :%v", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
+		d.kubectl.Error = err
+		return err
+	}
+	return d.kubectl.Error
+}
+func (d *deploy) Scale(replicas int32) error {
+	var item v1.Deployment
 
 	err := d.kubectl.Get(&item).Error
 	if err != nil {
-		klog.Errorf("Deployment Restart Get(&item) error :%v", err)
+		klog.Errorf("Deployment Scale Get %s/%s error :%v", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
 		d.kubectl.Error = err
 		return d.kubectl.Error
 	}
-
-	if item.Spec.Template.Annotations == nil {
-		item.Spec.Template.Annotations = map[string]string{}
-	}
-	item.Spec.Template.Annotations["kom.kubernetes.io/restartedAt"] = time.Now().Format(time.RFC3339)
-	err = d.kubectl.Update(&item).Error
+	patchData := fmt.Sprintf("{\"spec\":{\"replicas\":%d}}", replicas)
+	err = d.kubectl.Resource(&item).
+		Patch(&item, types.MergePatchType, patchData).Error
 	if err != nil {
-		klog.Errorf("Deployment Restart Update(&item) error :%v", err)
+		klog.Errorf("Deployment Scale %s/%s error :%v", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
 		d.kubectl.Error = err
-		return d.kubectl.Error
+		return err
 	}
 	return d.kubectl.Error
 }
