585165aa5706a06a0dba56711fdd4329f4a13453
weibaohui <weibaohui@yeah.net>
2024-11-24 17:12:45 +0800

    feat(kom): 添加 ReplicationController 缩放功能
    - 实现了 ReplicationController 的 Scale 方法，用于调整副本数量
    - 新增 replicationController 结构体和相关方法
    - 使用 kubectl 工具进行资源获取和更新操作

---
+++ kom/ctl_rc.go
@@ -0,0 +1,31 @@
+package kom
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+type replicationController struct {
+	kubectl *Kubectl
+}
+
+func (r *replicationController) Scale(replicas int32) error {
+	var item corev1.ReplicationController
+
+	err := r.kubectl.Get(&item).Error
+	if err != nil {
+		r.kubectl.Error = errors.Wrapf(err, "ReplicationController Scale Get %s/%s error", r.kubectl.Statement.Namespace, r.kubectl.Statement.Name)
+		return r.kubectl.Error
+	}
+	patchData := fmt.Sprintf("{\"spec\":{\"replicas\":%r}}", replicas)
+	err = r.kubectl.Resource(&item).
+		Patch(&item, types.MergePatchType, patchData).Error
+	if err != nil {
+		r.kubectl.Error = errors.Wrapf(err, "ReplicationController Scale %s/%s error", r.kubectl.Statement.Namespace, r.kubectl.Statement.Name)
+		return err
+	}
+	return r.kubectl.Error
+}
