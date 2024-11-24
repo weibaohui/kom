2afd69e9a50ebd673d28fc0ce73a5aadacdecdca
weibaohui <weibaohui@yeah.net>
2024-11-24 17:13:27 +0800

    refactor(kom): 重构 ctl.go 文件
    - 移除了 deploy 结构体和相关方法
    - 新增了 ReplicationController、ReplicaSet、StatefulSet 和 Pod 方法
    - 简化了文件结构，提高了代码可读性和可维护性

--- kom/ctl.go
+++ kom/ctl.go
@@ -1,75 +1,31 @@
 package kom
 
-import (
-	"fmt"
-	"time"
-
-	v1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/klog/v2"
-)
-
 type ctl struct {
 	kubectl *Kubectl
 }
 
-type deploy struct {
-	kubectl *Kubectl
-}
-
 func (c *ctl) Deployment() *deploy {
-
 	return &deploy{
 		kubectl: c.kubectl,
 	}
 }
-
-func (d *deploy) Restart() error {
-	var item v1.Deployment
-	err := d.kubectl.Get(&item).Error
-	if err != nil {
-		klog.Errorf("Deployment Restart %s/%s error :%v", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
-		d.kubectl.Error = err
-		return d.kubectl.Error
+func (c *ctl) ReplicationController() *replicationController {
+	return &replicationController{
+		kubectl: c.kubectl,
 	}
-	patchData := `{
-	"spec": {
-		"template": {
-			"metadata": {
-				"annotations": {
-						"kom.kubernetes.io/restartedAt": "%s"
-				}
-			}
-		}
-	}
-}`
-	patchData = fmt.Sprintf(patchData, time.Now().Format(time.DateTime))
-
-	err = d.kubectl.Resource(&item).
-		Patch(&item, types.MergePatchType, patchData).Error
-	if err != nil {
-		klog.Errorf("Deployment Restart %s/%s error :%v", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
-		d.kubectl.Error = err
-		return err
-	}
-	return d.kubectl.Error
 }
-func (d *deploy) Scale(replicas int32) error {
-	var item v1.Deployment
-
-	err := d.kubectl.Get(&item).Error
-	if err != nil {
-		klog.Errorf("Deployment Scale Get %s/%s error :%v", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
-		d.kubectl.Error = err
-		return d.kubectl.Error
+func (c *ctl) ReplicaSet() *replicaSet {
+	return &replicaSet{
+		kubectl: c.kubectl,
 	}
-	patchData := fmt.Sprintf("{\"spec\":{\"replicas\":%d}}", replicas)
-	err = d.kubectl.Resource(&item).
-		Patch(&item, types.MergePatchType, patchData).Error
-	if err != nil {
-		klog.Errorf("Deployment Scale %s/%s error :%v", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
-		d.kubectl.Error = err
-		return err
+}
+func (c *ctl) StatefulSet() *statefulSet {
+	return &statefulSet{
+		kubectl: c.kubectl,
 	}
-	return d.kubectl.Error
+}
+func (c *ctl) Pod() *pod {
+	return &pod{
+		kubectl: c.kubectl,
+	}
 }
