8b477de2774950c6b8fa646f657a348c602096c8
weibaohui <weibaohui@yeah.net>
2024-11-24 17:12:31 +0800

    feat(kom): 添加 StatefulSet 缩放功能
    - 新增 statefulSet 结构体和 Scale 方法
    - 实现了获取 StatefulSet 信息和更新副本数的功能
    - 添加了错误处理和日志记录

---
+++ kom/ctl_sts.go
@@ -0,0 +1,33 @@
+package kom
+
+import (
+	"fmt"
+
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
+)
+
+type statefulSet struct {
+	kubectl *Kubectl
+}
+
+func (s *statefulSet) Scale(replicas int32) error {
+	var item v1.StatefulSet
+
+	err := s.kubectl.Get(&item).Error
+	if err != nil {
+		klog.Errorf("StatefulSet Scale Get %s/%s error :%v", s.kubectl.Statement.Namespace, s.kubectl.Statement.Name, err)
+		s.kubectl.Error = err
+		return s.kubectl.Error
+	}
+	patchData := fmt.Sprintf("{\"spec\":{\"replicas\":%s}}", replicas)
+	err = s.kubectl.Resource(&item).
+		Patch(&item, types.MergePatchType, patchData).Error
+	if err != nil {
+		klog.Errorf("StatefulSet Scale %s/%s error :%v", s.kubectl.Statement.Namespace, s.kubectl.Statement.Name, err)
+		s.kubectl.Error = err
+		return err
+	}
+	return s.kubectl.Error
+}
