1685f31c4f36fa48a7d8e5b7b1fa78dcc8fe9c07
weibaohui <weibaohui@yeah.net>
2024-11-25 21:47:47 +0800

    feat(kom): 增加 rollout 暂停和恢复功能
    - 新增 Pause() 和 Resume() 方法，用于暂停和恢复 Deployment、StatefulSet、DaemonSet 和 ReplicaSet 的更新
    - 优化 Restart() 方法，增加日志记录和错误处理
    - 增加对不支持操作的资源类型的错误提示

--- kom/ctl_rollout.go
+++ kom/ctl_rollout.go
@@ -12,29 +12,68 @@ type rollout struct {
 	kubectl *Kubectl
 }
 
+func (d *rollout) logInfo(action string) {
+	kind := d.kubectl.Statement.GVK.Kind
+	resource := d.kubectl.Statement.GVR.Resource
+	namespace := d.kubectl.Statement.Namespace
+	name := d.kubectl.Statement.Name
+	klog.V(8).Infof("%s Kind=%s", action, kind)
+	klog.V(8).Infof("%s Resource=%s", action, resource)
+	klog.V(8).Infof("%s %s/%s", action, namespace, name)
+}
+func (d *rollout) handleError(kind string, namespace string, name string, action string, err error) error {
+	if err != nil {
+		d.kubectl.Error = fmt.Errorf("%s %s/%s %s error %v", kind, namespace, name, action, err)
+		return err
+	}
+	return nil
+}
+func (d *rollout) checkResourceKind(kind string) error {
+	if !(kind == "Deployment" || kind == "StatefulSet" || kind == "DaemonSet" || kind == "ReplicaSet") {
+		d.kubectl.Error = fmt.Errorf("%s %s/%s operation is not supported", kind, "<namespace>", "<name>")
+		return d.kubectl.Error
+	}
+	return nil
+}
 func (d *rollout) Restart() error {
 
 	kind := d.kubectl.Statement.GVK.Kind
-	klog.V(8).Infof("Restart Kind=%s", kind)
-	klog.V(8).Infof("Restart Resource=%s", d.kubectl.Statement.GVR.Resource)
-	klog.V(8).Infof("Restart %s/%s", d.kubectl.Statement.Namespace, d.kubectl.Statement.Name)
+	d.logInfo("Restart")
 
-	// 当前支持restart方法的资源有
-	// Deployment
-	// StatefulSet
-	// DaemonSet
-	// ReplicaSet
-	if !(kind == "Deployment" || kind == "StatefulSet" || kind == "DaemonSet" || kind == "ReplicaSet") {
-		d.kubectl.Error = fmt.Errorf("%s %s/%s restarting is not supported", kind, d.kubectl.Statement.Namespace, d.kubectl.Statement.Name)
-		return d.kubectl.Error
+	if err := d.checkResourceKind(kind); err != nil {
+		return err
 	}
 
 	var item interface{}
 	patchData := fmt.Sprintf(`{"spec":{"template":{"metadata":{"annotations":{"kom.kubernetes.io/restartedAt":"%s"}}}}}`, time.Now().Format(time.DateTime))
 	err := d.kubectl.Patch(&item, types.MergePatchType, patchData).Error
-	if err != nil {
-		d.kubectl.Error = fmt.Errorf("%s %s/%s restarting error %v", kind, d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, err)
+	return d.handleError(kind, d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, "restarting", err)
+}
+func (d *rollout) Pause() error {
+	kind := d.kubectl.Statement.GVK.Kind
+	d.logInfo("Restart")
+
+	if err := d.checkResourceKind(kind); err != nil {
 		return err
 	}
-	return d.kubectl.Error
+
+	var item interface{}
+	patchData := `{"spec":{"paused":true}}`
+	err := d.kubectl.Patch(&item, types.MergePatchType, patchData).Error
+	return d.handleError(kind, d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, "pause", err)
+
+}
+func (d *rollout) Resume() error {
+	kind := d.kubectl.Statement.GVK.Kind
+	d.logInfo("Restart")
+
+	if err := d.checkResourceKind(kind); err != nil {
+		return err
+	}
+
+	var item interface{}
+	patchData := `{"spec":{"paused":null}}`
+	err := d.kubectl.Patch(&item, types.MergePatchType, patchData).Error
+	return d.handleError(kind, d.kubectl.Statement.Namespace, d.kubectl.Statement.Name, "resume", err)
+
 }
