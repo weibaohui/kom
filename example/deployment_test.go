878fb281b040535d5ebc7a220731e61224cb8df1
weibaohui <weibaohui@yeah.net>
2024-11-26 17:59:16 +0800

    test(example): 添加 Deployment 历史记录测试功能
    - 新增 TestHistory 函数，用于测试 Deployment 的历史记录功能
    - 创建一个名为 nginx-history 的 Deployment
    -验证 Deployment 创建成功后，执行重启操作
    - 获取并打印 Deployment 的历史记录
    - 最后进行清理，删除创建的 Deployment

--- example/deployment_test.go
+++ example/deployment_test.go
@@ -91,6 +91,91 @@ func TestRestart(t *testing.T) {
 		t.Errorf("Deployment Restart Clean(&item) error :%v", err)
 	}
 }
+func TestHistory(t *testing.T) {
+	name := "nginx-history"
+	item := v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+			Labels: map[string]string{
+				"app": name,
+			},
+		},
+		Spec: v1.DeploymentSpec{
+			Replicas: utils.Int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": name,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  name,
+							Image: "nginx:1.14.2",
+						},
+					},
+				},
+			},
+		},
+	}
+	// 先创建Deploy
+	err := kom.DefaultCluster().
+		Resource(&item).
+		Create(&item).Error
+	if err != nil {
+		t.Errorf("Deployment Create(&item) error :%v", err)
+	}
+
+	if utils.WaitUntil(
+		func() bool {
+			var target v1.Deployment
+			kom.DefaultCluster().Resource(&target).
+				Namespace("default").
+				Name(name).
+				Get(&target)
+			if target.Spec.Template.Spec.Containers[0].Name == name {
+				// 达成测试条件
+				return true
+			}
+			return false
+		}, interval, timeout) {
+		t.Logf("创建Deploy nginx 成功")
+
+	} else {
+		t.Errorf("创建Deploy nginx 失败")
+		return
+	}
+
+	// 重启
+	err = kom.DefaultCluster().Resource(&item).
+		Namespace("default").Name(name).Ctl().Rollout().Restart()
+
+	history, err := kom.DefaultCluster().Resource(&v1.Deployment{}).
+		Namespace("default").Name(name).Ctl().Rollout().History()
+	t.Log(history)
+	t.Log(history)
+
+	if err != nil {
+		t.Errorf("Deployment History(&item) error :%v", err)
+	}
+	t.Logf("History Deploy nginx 成功")
+
+	// 清理
+	err = kom.DefaultCluster().Resource(&item).
+		Namespace("default").
+		Name(name).
+		Delete().Error
+	if err != nil {
+		t.Errorf("Deployment History Clean(&item) error :%v", err)
+	}
+}
 
 func TestScale(t *testing.T) {
 	name := "nginx-scale"
