cb82af1f6985327fee9dc7c2c9812dbcaacbb406
weibaohui <weibaohui@yeah.net>
2024-11-28 11:48:40 +0800

    test(example): 扩展 rollout测试覆盖 DaemonSet
    - 重命名 TestRollout_Undo 为 TestRollout_Deploy_Undo，

--- example/rollout_test.go
+++ example/rollout_test.go
@@ -7,7 +7,7 @@ import (
 	v1 "k8s.io/api/apps/v1"
 )
 
-func TestRollout_Undo(t *testing.T) {
+func TestRollout_Deploy_Undo(t *testing.T) {
 	result, err := kom.DefaultCluster().Resource(&v1.Deployment{}).
 		Namespace("default").Name("random-number-deployment").Ctl().Rollout().Undo(5)
 	if err != nil {
@@ -16,12 +16,30 @@ func TestRollout_Undo(t *testing.T) {
 	}
 	t.Logf("random-number-deployment undo %s", result)
 }
-func TestRollout_History(t *testing.T) {
+func TestRollout_Deploy_History(t *testing.T) {
 	result, err := kom.DefaultCluster().Resource(&v1.Deployment{}).
 		Namespace("default").Name("random-number-deployment").Ctl().Rollout().History()
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	t.Logf("random-number-deployment undo %s", result)
+	t.Logf("random-number-deployment rollout history %s", result)
+}
+func TestRollout_Ds_Undo(t *testing.T) {
+	result, err := kom.DefaultCluster().Resource(&v1.DaemonSet{}).
+		Namespace("default").Name("random-d-generator").Ctl().Rollout().Undo(0)
+	if err != nil {
+		t.Log(err)
+		return
+	}
+	t.Logf("random-d-generator undo %s", result)
+}
+func TestRollout_Ds_History(t *testing.T) {
+	result, err := kom.DefaultCluster().Resource(&v1.DaemonSet{}).
+		Namespace("default").Name("random-d-generator").Ctl().Rollout().History()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("random-d-generator rollout %s", result)
 }
