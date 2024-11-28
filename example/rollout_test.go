1c96200377132e2333cfd0a9c841ee9bb039b362
weibaohui <weibaohui@yeah.net>
2024-11-28 17:26:22 +0800

    test(example): 重构 rollout 测试用例
    - 新增 prepareDeploy 函数用于部署测试资源
    - 为 Deployment 和 DaemonSet增加可重用的变量- 更新测试函数中的硬编码名称，使用变量替代
    - 优化测试输出日志的格式

--- example/rollout_test.go
+++ example/rollout_test.go
@@ -7,48 +7,125 @@ import (
 	v1 "k8s.io/api/apps/v1"
 )
 
+func prepareDeploy() {
+	var yaml = `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: random-number-deployment
+  namespace: default
+spec:
+  selector:
+    matchLabels:
+      app: random-number
+  template:
+    metadata:
+      labels:
+        app: random-number
+    spec:
+      containers:
+      - args:
+        - |
+          while true; do
+            echo "A+$RANDOM";
+            sleep 2;
+          done;
+        command:
+        - /bin/sh
+        - -c
+        image: busybox
+        imagePullPolicy: Always
+        name: random-number
+        resources:
+          limits:
+            cpu: 50m
+            memory: 16Mi
+          requests:
+            cpu: 10m
+            memory: 8Mi
+---
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: random-d-generator
+  namespace: default
+spec:
+  selector:
+    matchLabels:
+      app: random-d-generator
+  template:
+    metadata:
+      labels:
+        app: random-d-generator
+    spec:
+      containers:
+      - command:
+        - /bin/sh
+        - -c
+        - |
+          while true; do
+            echo "D$(head /dev/urandom | tr -dc '0-9' | head -c 5)";
+            sleep 2;
+          done
+        image: busybox
+        imagePullPolicy: Always
+        name: busybox
+        resources:
+          limits:
+            cpu: 50m
+            memory: 16Mi
+          requests:
+            cpu: 10m
+            memory: 8Mi
+`
+	kom.DefaultCluster().Applier().Apply(yaml)
+}
+
+var deployName = "random-number-deployment"
+var dsName = "random-d-generator"
+
 func TestRollout_Deploy_Undo(t *testing.T) {
 	result, err := kom.DefaultCluster().Resource(&v1.Deployment{}).
-		Namespace("default").Name("random-number-deployment").Ctl().Rollout().Undo(5)
+		Namespace("default").Name(deployName).Ctl().Rollout().Undo()
 	if err != nil {
 		t.Log(err)
 		return
 	}
-	t.Logf("random-number-deployment undo %s", result)
+	t.Logf("%s undo %s", deployName, result)
 }
 func TestRollout_Deploy_History(t *testing.T) {
 	result, err := kom.DefaultCluster().Resource(&v1.Deployment{}).
-		Namespace("default").Name("random-number-deployment").Ctl().Rollout().History()
+		Namespace("default").Name(deployName).Ctl().Rollout().History()
 	if err != nil {
 		t.Log(err)
 		return
 	}
-	t.Logf("random-number-deployment rollout history %s", result)
+	t.Logf("%s rollout history %s", deployName, result)
 }
 func TestRollout_Ds_Undo(t *testing.T) {
 	result, err := kom.DefaultCluster().Resource(&v1.DaemonSet{}).
-		Namespace("default").Name("random-d-generator").Ctl().Rollout().Undo(0)
+		Namespace("default").Name(dsName).Ctl().Rollout().Undo()
 	if err != nil {
 		t.Log(err)
 		return
 	}
-	t.Logf("random-d-generator undo %s", result)
+	t.Logf("%s undo %s", dsName, result)
 }
 func TestRollout_Ds_History(t *testing.T) {
 	result, err := kom.DefaultCluster().Resource(&v1.DaemonSet{}).
-		Namespace("default").Name("random-d-generator").Ctl().Rollout().History()
+		Namespace("default").Name(dsName).Ctl().Rollout().History()
 	if err != nil {
 		t.Log(err)
 		return
 	}
-	t.Logf("random-d-generator rollout %s", result)
+	t.Logf("%s rollout %s", dsName, result)
 }
 func TestRollout_Ds_Status(t *testing.T) {
 	result, err := kom.DefaultCluster().Resource(&v1.DaemonSet{}).
-		Namespace("default").Name("random-d-generator").Ctl().Rollout().Status()
+		Namespace("default").Name(dsName).Ctl().Rollout().Status()
 	if err != nil {
 		t.Log(err)
 		return
 	}
-	t.Logf("random-d-generator rollout %s", result)
+	t.Logf("%s rollout %s", dsName, result)
 }
