e5c16f4fd0d6486d36490f2ad2fc82d9da415f5c
weibaohui <weibaohui@yeah.net>
2024-12-11 18:12:21 +0800

    test(example): 添加 SQL 查询测试用例
    - 新增 sql_test.go 文件，实现对 SQL 查询功能的测试
    - 测试了多种 SQL 查询条件，包括等值、不等值、范围、模糊匹配等
    - 验证了对不同资源类型（如 deploy、virtualmachine）的查询
    - 检查了查询结果的正确性和完整性

---
+++ example/sql_test.go
@@ -0,0 +1,47 @@
+package example
+
+import (
+	"testing"
+
+	"github.com/weibaohui/kom/kom"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func TestSQL(t *testing.T) {
+	sql := "select * from deploy where id!=1 and `x.y.z.name`!='xxx' and (`yyy.uuu.ii.sage`>80 and sex=0) and (x='ttt' or yyy='ttt') order by id desc"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `metadata.name`!='hello-28890812-wj2dh' order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`>=1 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`>=2 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) And  `metadata.name` Like '%dns%' order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `metadata.name` LIKE '%dns' order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `metadata.name` LIKE 'dns%' order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`<>2 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`!=2 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`>1 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`<1 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`<3 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`=3 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`=2 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`in (2,1) order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas` not in (2,3) order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas` between 3 and 4 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas` not between 3 and 4 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas` not between 1 and 2 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas` not between 2 and 3 order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `metadata.creationTimestamp`  between '2024-11-09 00:00:00' and '2024-11-09 23:59:39' order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `metadata.creationTimestamp` not between '2024-11-08' and '2024-11-10' order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `metadata.creationTimestamp` > '2024-11-08' order by id desc limit 2"
+	sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `spec.replicas`in (2,1) order by id desc limit 2"
+	sql = "select * from virtualmachine where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' )  order by id desc"
+	// sql = "select * from deploy where (`metadata.namespace`='kube-system' or `metadata.namespace`='default' ) and  `metadata.creationTimestamp` in ('2024-11-08','2024-11-09','2024-11-10') order by id desc limit 2"
+
+	var list []unstructured.Unstructured
+	err := kom.DefaultCluster().Sql(sql).List(&list).Error
+	if err != nil {
+		t.Logf("List error %v", err)
+	}
+	t.Logf("Count %d", len(list))
+	for _, d := range list {
+		t.Logf("List Items foreach %s,%s\n", d.GetNamespace(), d.GetName())
+	}
+}
