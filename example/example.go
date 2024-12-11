529f77b89e632119bd94bd5b1bf0e264986f89de
weibaohui <weibaohui@yeah.net>
2024-12-11 21:24:19 +0800

    feat(example): 添加 SQL 查询功能- 新增 sql() 函数，用于执行 SQL 查询 - 查询 kube-system 和 default 命名空间中的 pod，并按创建时间排序- 打印查询结果的总数和每个项目的详细信息

--- example/example.go
+++ example/example.go
@@ -35,7 +35,21 @@ func Example() {
 	// podCommand()
 	// podFileCommand()
 	// podLogs()
+	// sql()
 
+}
+func sql() {
+	sql := "select * from pod where `metadata.namespace`='kube-system' or `metadata.namespace`='default' order by  `metadata.creationTimestamp` asc   "
+
+	var list []v1.Deployment
+	err := kom.DefaultCluster().Sql(sql).List(&list).Error
+	if err != nil {
+		fmt.Printf("List error %v", err)
+	}
+	fmt.Printf("Count %d", len(list))
+	for _, d := range list {
+		fmt.Printf("List Items foreach %s,%s at %s \n", d.GetNamespace(), d.GetName(), d.GetCreationTimestamp())
+	}
 }
 
 func drainNode() {
