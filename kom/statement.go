857b9feeb40f9f82153ea41b86510dcd00c36fa5
weibaohui <weibaohui@yeah.net>
2024-12-12 11:16:21 +0800

    feat(statement): 增加原始 SQL 和解析状态字段
    - 在 Statement 结构中添加 Sql 字段，用于存储原始 SQL 语句
    - 添加 Parsed 字段，表示 SQL 语句是否已被解析
    - 新增 From 字段，用于存储 FROM 子句中的表名

--- kom/statement.go
+++ kom/statement.go
@@ -43,6 +43,9 @@ type Filter struct {
 	Order      string      `json:"order,omitempty"`
 	Limit      int         `json:"limit,omitempty"`
 	Offset     int         `json:"offset,omitempty"`
+	Sql        string      `json:"sql,omitempty"`    // 原始sql
+	Parsed     bool        `json:"parsed,omitempty"` // 是否解析过
+	From       string      `json:"from,omitempty"`   // From TableName
 }
 type Condition struct {
 	Depth     int
