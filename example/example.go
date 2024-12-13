e5f807918b68527b67711e14e3c948dd962ebeca
weibaohui <weibaohui@yeah.net>
2024-12-13 14:32:48 +0800

    refactor(example): 优化 SQL 查询语句并使用参数化查询- 将硬编码的 SQL 查询语句改为参数化查询，提高代码可维护性 - 使用占位符 ? 替代直接拼接 SQL 字符串，提升安全性 - 添加 "kube-system" 和 "default" 命名空间作为查询参数

--- example/example.go
+++ example/example.go
@@ -40,10 +40,10 @@ func Example() {
 
 }
 func sql() {
-	sql := "select * from pod where metadata.namespace='kube-system' or metadata.namespace='default'    order by  metadata.creationTimestamp desc "
+	sql := "select * from pod where metadata.namespace=? or metadata.namespace=?    order by  metadata.creationTimestamp desc "
 
 	var list []corev1.Pod
-	err := kom.DefaultCluster().Sql(sql).List(&list).Error
+	err := kom.DefaultCluster().Sql(sql, "kube-system", "default").List(&list).Error
 	if err != nil {
 		fmt.Printf("List error %v", err)
 	}
