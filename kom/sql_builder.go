2071d50cc5223f5c7209d4ce71e0d84e437b8b11
weibaohui <weibaohui@yeah.net>
2024-12-13 14:16:11 +0800

    feat(kom): 优化 SQL语句解析和执行
    - 新增 formatSql 函数，用于格式化 SQL 语句中的参数- 在 Sql 和 Where 方法中使用 formatSql 函数，提高 SQL语句的灵活性和安全性
    - 添加反引号处理，将 metadata

--- kom/sql_builder.go
+++ kom/sql_builder.go
@@ -10,14 +10,20 @@ import (
 	"k8s.io/klog/v2"
 )
 
-// Sql TODO 解析sql为函数调用，实现支持原生sql语句
+// Sql TODO Insert Update Delete
+//
+//	 已支持Select
+//		解析sql为函数调用，实现支持原生sql语句
+//
 // select * from pod where pod.name='?', 'abc'
 func (k *Kubectl) Sql(sql string, values ...interface{}) *Kubectl {
 	tx := k.getInstance()
 	tx.AllNamespace()
 
-	// // 添加反引号
-	// sql = AddBackticks(sql)
+	sql = formatSql(sql, values)
+	// 添加反引号，将metadata.name 转为`metadata.name`,
+	// k8s中很多类似json的字段，需要用反引号进行包裹，避免被作为db.table形式使用
+	sql = NewSqlParse(sql).AddBackticks()
 
 	stmt, err := sqlparser.Parse(sql)
 	if err != nil {
@@ -94,23 +100,13 @@ func (k *Kubectl) From(tableName string) *Kubectl {
 func (k *Kubectl) Where(condition string, values ...interface{}) *Kubectl {
 	tx := k.getInstance()
 
-	// 组装成一个sql语句，调用SQL方法中的解析
-	// 如何能将condition,values 合并成一个SQL？
-
-	// 将 values 替换到 condition 中的占位符 ?
-	finalCondition := condition
-	for _, value := range values {
-		// 对值进行安全格式化，例如字符串加单引号
-		switch v := value.(type) {
-		case string:
-			finalCondition = strings.Replace(finalCondition, "?", fmt.Sprintf("'%s'", v), 1)
-		default:
-			finalCondition = strings.Replace(finalCondition, "?", fmt.Sprintf("%v", v), 1)
-		}
-	}
+	sql := formatSql(condition, values)
 
 	// 组装完整的 SQL
-	sql := fmt.Sprintf("SELECT * FROM fake WHERE %s", finalCondition)
+	sql = fmt.Sprintf("SELECT * FROM fake WHERE %s", sql)
+	// 添加反引号，将metadata.name 转为`metadata.name`,
+	// k8s中很多类似json的字段，需要用反引号进行包裹，避免被作为db.table形式使用
+	sql = NewSqlParse(sql).AddBackticks()
 
 	tx.Statement.Filter.Sql = sql
 
@@ -128,7 +124,9 @@ func (k *Kubectl) Where(condition string, values ...interface{}) *Kubectl {
 	// 断言为 *sqlparser.Select 类型
 	selectStmt, ok := stmt.(*sqlparser.Select)
 	if !ok {
-		log.Fatalf("Not a SELECT statement")
+		klog.Errorf("not select parsing SQL:%s,%v", sql, err)
+		tx.Error = err
+		return tx
 	}
 
 	// 解析Where语句，活的执行条件
@@ -143,6 +141,23 @@ func (k *Kubectl) Where(condition string, values ...interface{}) *Kubectl {
 	tx.Statement.Filter.Parsed = true
 
 	return tx
+}
+
+// formatSql
+//
+//	select * from pod where pod.name='?', 'abc'
+func formatSql(condition string, values []interface{}) string {
+	// 将 values 替换到 condition 中的占位符 ?
+	for _, value := range values {
+		// 对值进行安全格式化，例如字符串加单引号
+		switch v := value.(type) {
+		case string:
+			condition = strings.Replace(condition, "?", fmt.Sprintf("'%s'", v), 1)
+		default:
+			condition = strings.Replace(condition, "?", fmt.Sprintf("%v", v), 1)
+		}
+	}
+	return condition
 }
 
 func (k *Kubectl) Order(order string) *Kubectl {
