5f1a81b316624447809944988cd1038695ce3778
weibaohui <weibaohui@yeah.net>
2024-12-15 18:18:33 +0800

    test(example): 添加 SQL 查询功能测试用例
    - 新增 select_func

--- example/example.go
+++ example/example.go
@@ -35,30 +35,14 @@ func Example() {
 	// podCommand()
 	// podFileCommand()
 	// podLogs()
-	// komFuncSql()
 	// sql()
 
 }
 func sql() {
-	sql := "select * from pod where metadata.namespace=? or metadata.namespace=?    order by  metadata.creationTimestamp desc "
+	sql := "select * from pod where metadata.namespace=? or metadata.namespace=?  and(x.x='x' or y.y='y')   order by  metadata.creationTimestamp desc "
 
 	var list []corev1.Pod
 	err := kom.DefaultCluster().Sql(sql, "kube-system", "default").List(&list).Error
-	if err != nil {
-		fmt.Printf("List error %v", err)
-	}
-	fmt.Printf("Count %d\n", len(list))
-	for _, d := range list {
-		fmt.Printf("List Item  %s\t %s  \t %s \n", d.GetNamespace(), d.GetName(), d.GetCreationTimestamp())
-	}
-}
-func komFuncSql() {
-	var list []corev1.Pod
-	err := kom.DefaultCluster().From("pod").
-		Where("`metadata.namespace` =?  or `metadata.namespace`=?", "kube-system", "default").
-		Order("`metadata.creationTimestamp` desc").
-		List(&list).Error
-
 	if err != nil {
 		fmt.Printf("List error %v", err)
 	}
