17b7945a7419bcb3aa14509712f7c4d841b91aa3
weibaohui <weibaohui@yeah.net>
2024-12-30 12:06:05 +0800

    feat(pod): 添加资源使用情况获取功能
    - 新增 ResourceUsage 方法，获取 Pod 的资源使用情况
    - 计算 CPU 和内存的请求和限制占比
    - 返回 ResourceUsageResult 结构体，包含资源请求、限制、可分配量和使用占比

--- kom/ctl_pod.go
+++ kom/ctl_pod.go
@@ -11,6 +11,7 @@ import (
 	"github.com/weibaohui/kom/utils"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
+	resourcehelper "k8s.io/kubectl/pkg/util/resource"
 )
 
 type pod struct {
@@ -274,4 +275,64 @@ func parseFileList(path, output string) []*FileInfo {
 	}
 
 	return nodes
+}
+
+// ResourceUsage 获取节点的资源使用情况，包括资源的请求和限制，还有当前使用占比
+func (p *pod) ResourceUsage() *ResourceUsageResult {
+
+	var inst *v1.Pod
+	err := p.kubectl.newInstance().Resource(&v1.Pod{}).
+		Namespace(p.kubectl.Statement.Namespace).
+		Name(p.kubectl.Statement.Name).
+		Get(&inst).Error
+	if err != nil {
+		klog.V(6).Infof("Get ResourceUsage in pod/%s  error %v\n", p.kubectl.Statement.Name, err.Error())
+		return nil
+	}
+	req, limit := resourcehelper.PodRequestsAndLimits(inst)
+	if req == nil || limit == nil {
+		return nil
+	}
+	nodeName := inst.Spec.NodeName
+
+	var n *v1.Node
+	err = p.kubectl.newInstance().Resource(&v1.Node{}).
+		Name(nodeName).Get(&n).Error
+	if err != nil {
+		klog.V(6).Infof("Get Pod ResourceUsage in node/%s  error %v\n", nodeName, err.Error())
+		return nil
+	}
+
+	allocatable := n.Status.Capacity
+	if len(n.Status.Allocatable) > 0 {
+		allocatable = n.Status.Allocatable
+	}
+
+	klog.V(6).Infof("allocatable=:\n%s", utils.ToJSON(allocatable))
+	cpuReq, cpuLimit, memoryReq, memoryLimit := req[v1.ResourceCPU], limit[v1.ResourceCPU], req[v1.ResourceMemory], limit[v1.ResourceMemory]
+	fractionCpuReq := float64(cpuReq.MilliValue()) / float64(allocatable.Cpu().MilliValue()) * 100
+	fractionCpuLimit := float64(cpuLimit.MilliValue()) / float64(allocatable.Cpu().MilliValue()) * 100
+	fractionMemoryReq := float64(memoryReq.Value()) / float64(allocatable.Memory().Value()) * 100
+	fractionMemoryLimit := float64(memoryLimit.Value()) / float64(allocatable.Memory().Value()) * 100
+
+	usageFractions := map[v1.ResourceName]ResourceUsageFraction{
+		v1.ResourceCPU: {
+			RequestFraction: fractionCpuReq,
+			LimitFraction:   fractionCpuLimit,
+		},
+		v1.ResourceMemory: {
+			RequestFraction: fractionMemoryReq,
+			LimitFraction:   fractionMemoryLimit,
+		},
+	}
+
+	klog.V(6).Infof("%s\t%s\t\t%s (%d%%)\t%s (%d%%)\t%s (%d%%)\t%s (%d%%)\n", inst.Namespace, inst.Name,
+		cpuReq.String(), int64(fractionCpuReq), cpuLimit.String(), int64(fractionCpuLimit),
+		memoryReq.String(), int64(fractionMemoryReq), memoryLimit.String(), int64(fractionMemoryLimit))
+	return &ResourceUsageResult{
+		Requests:       req,
+		Limits:         limit,
+		Allocatable:    allocatable,
+		UsageFractions: usageFractions,
+	}
 }
