8c5a7f7eba4ab887418a5ed87ec7c55093d4fdc7
weibaohui <weibaohui@yeah.net>
2025-01-02 21:32:41 +0800

    feat(statement): add TotalRows field to Statement struct for pagination support
    - Introduced a new TotalRows field in the Statement struct to store the total number of rows for pagination purposes.
    - This addition enhances the functionality of the List query method, providing better insights into the total number of items available.

--- kom/statement.go
+++ kom/statement.go
@@ -5,7 +5,7 @@ import (
 	"io"
 	"time"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -18,6 +18,7 @@ import (
 type Statement struct {
 	*Kubectl            `json:"Kubectl,omitempty"`  // 基础配置
 	RowsAffected        int64                       `json:"rowsAffected,omitempty"`        // 返回受影响的行数
+	TotalRows           *int64                      `json:"totalRows,omitempty"`           // 返回查询总数，分页使用。只在List查询列表方法中生效。
 	AllNamespace        bool                        `json:"allNamespace,omitempty"`        // 所有名空间
 	Namespace           string                      `json:"namespace,omitempty"`           // 资源所属命名空间
 	Name                string                      `json:"name,omitempty"`                // 资源名称
