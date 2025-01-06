b392398451d3d94649df5309026bb92b7923d3b7
weibaohui <weibaohui@yeah.net>
2025-01-06 15:13:24 +0800

    refactor(callbacks): 重构 RegisterDefaultCallbacks 函数
    - 将函数参数从 *kom.ClusterInstances改为 *kom.ClusterInst，简化了函数签名
    - 移除了不必要的循环，直接对单个集群实例进行操作
    - 保留了原有的回调注册逻辑，确保功能不变

--- callbacks/callbacks.go
+++ callbacks/callbacks.go
@@ -15,49 +15,46 @@ func RegisterInit() {
 func init() {
 	RegisterInit()
 }
-func RegisterDefaultCallbacks(clusters *kom.ClusterInstances) func() {
+func RegisterDefaultCallbacks(c *kom.ClusterInst) func() {
 
-	all := clusters.AllClusters()
-	for _, c := range all {
-		klog.V(4).Infof("RegisterDefaultCallbacks for cluster %s", c.ID)
-		prefix := c.ID
+	klog.V(4).Infof("RegisterDefaultCallbacks for cluster %s", c.ID)
+	prefix := c.ID
 
-		// 为每一个集群进行注册
-		k := c.Kubectl
+	// 为每一个集群进行注册
+	k := c.Kubectl
 
-		queryCallback := k.Callback().Get()
-		_ = queryCallback.Register(fmt.Sprintf("%s:get", prefix), Get)
+	queryCallback := k.Callback().Get()
+	_ = queryCallback.Register(fmt.Sprintf("%s:get", prefix), Get)
 
-		listCallback := k.Callback().List()
-		_ = listCallback.Register(fmt.Sprintf("%s:list", prefix), List)
+	listCallback := k.Callback().List()
+	_ = listCallback.Register(fmt.Sprintf("%s:list", prefix), List)
 
-		watchCallback := k.Callback().Watch()
-		_ = watchCallback.Register(fmt.Sprintf("%s:watch", prefix), Watch)
+	watchCallback := k.Callback().Watch()
+	_ = watchCallback.Register(fmt.Sprintf("%s:watch", prefix), Watch)
 
-		createCallback := k.Callback().Create()
-		_ = createCallback.Register(fmt.Sprintf("%s:create", prefix), Create)
+	createCallback := k.Callback().Create()
+	_ = createCallback.Register(fmt.Sprintf("%s:create", prefix), Create)
 
-		updateCallback := k.Callback().Update()
-		_ = updateCallback.Register(fmt.Sprintf("%s:update", prefix), Update)
+	updateCallback := k.Callback().Update()
+	_ = updateCallback.Register(fmt.Sprintf("%s:update", prefix), Update)
 
-		patchCallback := k.Callback().Patch()
-		_ = patchCallback.Register(fmt.Sprintf("%s:patch", prefix), Patch)
+	patchCallback := k.Callback().Patch()
+	_ = patchCallback.Register(fmt.Sprintf("%s:patch", prefix), Patch)
 
-		deleteCallback := k.Callback().Delete()
-		_ = deleteCallback.Register(fmt.Sprintf("%s:delete", prefix), Delete)
+	deleteCallback := k.Callback().Delete()
+	_ = deleteCallback.Register(fmt.Sprintf("%s:delete", prefix), Delete)
 
-		execCallback := k.Callback().Exec()
-		_ = execCallback.Register(fmt.Sprintf("%s:pod:exec", prefix), ExecuteCommand)
+	execCallback := k.Callback().Exec()
+	_ = execCallback.Register(fmt.Sprintf("%s:pod:exec", prefix), ExecuteCommand)
 
-		streamExecCallback := k.Callback().StreamExec()
-		_ = streamExecCallback.Register(fmt.Sprintf("%s:pod:stream:exec", prefix), StreamExecuteCommand)
+	streamExecCallback := k.Callback().StreamExec()
+	_ = streamExecCallback.Register(fmt.Sprintf("%s:pod:stream:exec", prefix), StreamExecuteCommand)
 
-		logsCallback := k.Callback().Logs()
-		_ = logsCallback.Register(fmt.Sprintf("%s:pod:logs", prefix), GetLogs)
+	logsCallback := k.Callback().Logs()
+	_ = logsCallback.Register(fmt.Sprintf("%s:pod:logs", prefix), GetLogs)
 
-		describeCallback := k.Callback().Describe()
-		_ = describeCallback.Register(fmt.Sprintf("%s:describe", prefix), Describe)
-	}
+	describeCallback := k.Callback().Describe()
+	_ = describeCallback.Register(fmt.Sprintf("%s:describe", prefix), Describe)
 
 	return nil
 }
