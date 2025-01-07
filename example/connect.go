07f633056b1daa0203dbd1f1f20c041620ba3e41
weibaohui <weibaohui@yeah.net>
2025-01-07 10:34:47 +0800

    feat(example): 添加回调初始化并优化连接逻辑
    - 在 connect.go 中添加回调初始化，确保在注册集群前完成初始化
    - 移除 InCluster 注册方式，只保留通过配置文件路径注册集群的方式- 优化代码结构，提高可读性和可维护性

--- example/connect.go
+++ example/connect.go
@@ -4,16 +4,18 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/weibaohui/kom/callbacks"
 	"github.com/weibaohui/kom/kom"
 	"k8s.io/client-go/util/homedir"
 )
 
 func Connect() {
+	callbacks.RegisterInit()
+
 	defaultKubeConfig := os.Getenv("KUBECONFIG")
 	if defaultKubeConfig == "" {
 		defaultKubeConfig = filepath.Join(homedir.HomeDir(), ".kube", "config")
 	}
-	_, _ = kom.Clusters().RegisterInCluster()
 	_, _ = kom.Clusters().RegisterByPathWithID(defaultKubeConfig, "default")
 	kom.Clusters().Show()
 }
