25dc8861f7b9762c630835999fbd36b60daff521
weibaohui <weibaohui@yeah.net>
2025-01-07 10:34:57 +0800

    feat: 添加日志记录功能- 引入 klog 包用于日志记录 - 初始化日志标志并设置日志级别为 8 - 调用 example 包中的 Connect 和 Example函数

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/weibaohui/kom/example"
+	"k8s.io/klog/v2"
+)
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Set("v", "8")
+	example.Connect()
+	example.Example()
 }
