616b66e542cc50b6699ce35e269ec3a03832115a
weibaohui <weibaohui@yeah.net>
2025-01-07 10:49:31 +0800

    test(example): 优化 Pod 关联 Service 测试案例
    - 添加 YAML 配置，创建测试用的 Pod、Service 和 Ingress资源
    - 修改测试逻辑，使用新创建的资源进行测试- 增加对关联 Service 名称列表的检查

--- example/pod_link_test.go
+++ example/pod_link_test.go
@@ -1,16 +1,67 @@
 package example
 
 import (
+	"slices"
 	"testing"
+	"time"
 
 	"github.com/weibaohui/kom/kom"
 	v1 "k8s.io/api/core/v1"
 )
 
 func TestPodLink(t *testing.T) {
+	yaml := `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: nginx-pod
+  labels:
+    app: nginx
+spec:
+  containers:
+  - name: nginx
+    image: nginx:1.23.3
+    ports:
+    - containerPort: 80
+
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx-service
+spec:
+  selector:
+    app: nginx
+  ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 80
+  type: ClusterIP
+---
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: nginx-ingress
+  annotations:
+    nginx.ingress.kubernetes.io/rewrite-target: /
+spec:
+  rules:
+  - host: yourdomain.com
+    http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service:
+            name: nginx-service
+            port:
+              number: 80
+`
+	kom.DefaultCluster().Applier().Apply(yaml)
+	time.Sleep(10 * time.Second)
 	services, err := kom.DefaultCluster().Resource(&v1.Pod{}).
-		Namespace("k8m").
-		Name("k8m-6b56d66cbf-cf222").Ctl().Pod().LinkedService()
+		Namespace("default").
+		Name("nginx-pod").Ctl().Pod().LinkedService()
 	if err != nil {
 		t.Logf("get pod linked service error %v\n", err.Error())
 		return
@@ -19,4 +70,14 @@ func TestPodLink(t *testing.T) {
 		t.Logf("service name %v\n", service.Name)
 	}
 
+	//获取serviceNames列表
+	serviceNames := []string{}
+	for _, service := range services {
+		serviceNames = append(serviceNames, service.Name)
+	}
+	t.Logf("serviceNames %v\n", serviceNames)
+	//检查serviceNames列表是否包含nginx-service
+	if !slices.Contains(serviceNames, "nginx-service") {
+		t.Errorf("nginx-service not found in serviceNames")
+	}
 }
