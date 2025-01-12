5292ce60fe9134f3db667237f8eb85edfc714998
weibaohui <weibaohui@yeah.net>
2025-01-12 15:14:18 +0800

    test(example): 添加文档测试用例
    - 新增 TestDoc 函数，用于测试文档获取功能
    - 使用 kom.DefaultCluster().Status().Docs() 获取文档集合
    - 通过 FetchByGVK 方法获取特定资源的文档信息
    - 使用 utils.ToJSON 函数将文档内容格式化为 JSON 字符串

---
+++ example/doc_test.go
@@ -0,0 +1,14 @@
+package example
+
+import (
+	"testing"
+
+	"github.com/weibaohui/kom/kom"
+	"github.com/weibaohui/kom/utils"
+)
+
+func TestDoc(t *testing.T) {
+	docs := kom.DefaultCluster().Status().Docs()
+	pc := docs.FetchByGVK("v1", "Pod")
+	t.Logf(utils.ToJSON(pc))
+}
