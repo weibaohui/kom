5cd5556494754c7305e287ad41833b9acedab761
weibaohui <weibaohui@yeah.net>
2025-01-14 15:45:54 +0800

    refactor(parser): 增加对复杂字段的支持。 status.addresses[type=InternalIP].address 方便对列表的处理

--- example/sql_test.go
+++ example/sql_test.go
@@ -31,9 +31,9 @@ func TestSQL(t *testing.T) {
 	sql = "select * from deploy where (metadata.namespace='kube-system' or metadata.namespace='default' ) and  spec.replicas not between 2 and 3 order by id desc limit 2"
 	sql = "select * from deploy where (metadata.namespace='kube-system' or metadata.namespace='default' ) and  metadata.creationTimestamp  between '2024-11-09 00:00:00' and '2024-11-09 23:59:39' order by id desc limit 2"
 	sql = "select * from deploy where (metadata.namespace='kube-system' or metadata.namespace='default' ) and  metadata.creationTimestamp not between '2024-11-08' and '2024-11-10' order by id desc limit 2"
-	sql = "select * from deploy where (metadata.namespace='kube-system' or metadata.namespace='default' ) and  metadata.creationTimestamp > '2024-11-08' order by id desc limit 2"
-	sql = "select * from deploy where (metadata.namespace='kube-system' or metadata.namespace='default' ) and  spec.replicasin (2,1) order by id desc limit 2"
-	sql = "select * from virtualmachine where (metadata.namespace='kube-system' or metadata.namespace='default' )  order by id desc"
+	// sql = "select * from deploy where (metadata.namespace='kube-system' or metadata.namespace='default' ) and  metadata.creationTimestamp > '2024-11-08' order by id desc limit 2"
+	// sql = "select * from deploy where (metadata.namespace='kube-system' or metadata.namespace='default' ) and  spec.replicas in (2,1) order by id desc limit 2"
+	// sql = "select * from virtualmachine where (metadata.namespace='kube-system' or metadata.namespace='default' )  order by id desc"
 	// sql = "select * from deploy where (metadata.namespace='kube-system' or metadata.namespace='default' ) and  metadata.creationTimestamp in ('2024-11-08','2024-11-09','2024-11-10') order by id desc limit 2"
 
 	var list []unstructured.Unstructured
@@ -63,6 +63,20 @@ func TestPodSQL(t *testing.T) {
 	sql := "select * from pod where metadata.namespace='kube-system' or metadata.namespace='default'  order by metadata.name desc  "
 
 	var list []v1.Pod
+	err := kom.DefaultCluster().Sql(sql).List(&list).Error
+	if err != nil {
+		t.Logf("List error %v", err)
+	}
+	t.Logf("Count %d", len(list))
+	for _, d := range list {
+		t.Logf("List Items foreach %s,%s\n", d.GetNamespace(), d.GetName())
+	}
+}
+func TestNodeIPSQL(t *testing.T) {
+	sql := "select * from node where status.addresses[type=InternalIP].address like '%10%'  "
+	// sql = "select * from node where status.addresses.address like '%10%'  "
+
+	var list []v1.Node
 	err := kom.DefaultCluster().Sql(sql).List(&list).Error
 	if err != nil {
 		t.Logf("List error %v", err)
