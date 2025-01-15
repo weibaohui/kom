da1ef78063883f03df17832646574b087dee4703
weibaohui <weibaohui@yeah.net>
2025-01-15 22:41:54 +0800

    feat(kom): 添加停止和恢复工作负载的功能
    - 在 deploy、replicationController、replicaSet 和 statefulSet 类型中添加 Stop 和 Restore 方法
    - 实现 scale 类型的 Stop 和 Restore 方法，用于具体操作
    - 停止时将当前副本数记录到 annotation 中，恢复时从 annotation 中读取副本数
    - 支持 Deployment、StatefulSet、ReplicationController 和 ReplicaSet 类型的工作负载

---
+++ example/stop_restore_test.go
@@ -0,0 +1,56 @@
+package example
+
+import (
+	"testing"
+
+	"github.com/weibaohui/kom/kom"
+	v1 "k8s.io/api/apps/v1"
+)
+
+func TestDeployStop(t *testing.T) {
+	yaml := `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deployment
+  labels:
+    app: nginx
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx
+`
+	result := kom.DefaultCluster().Applier().Apply(yaml)
+	for _, r := range result {
+		t.Log(r)
+	}
+	var deploy v1.Deployment
+	err := kom.DefaultCluster().Resource(&deploy).
+		Namespace("default").
+		Name("nginx-deployment").
+		Ctl().Deployment().Stop()
+	if err != nil {
+		t.Log(err)
+	}
+}
+
+func TestDeployRestore(t *testing.T) {
+
+	var deploy v1.Deployment
+	err := kom.DefaultCluster().Resource(&deploy).
+		Namespace("default").
+		Name("nginx-deployment").
+		Ctl().Deployment().Restore()
+	if err != nil {
+		t.Log(err)
+	}
+}
