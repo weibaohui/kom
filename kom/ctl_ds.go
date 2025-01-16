9d4a0aa0542aba8276df529259d83cc01ccb9738
weibaohui <weibaohui@yeah.net>
2025-01-16 15:58:50 +0800

    feat(kom): 添加停止和恢复 DaemonSet 功能
    - 新增 Stop 方法，通过修改 nodeSelector将 DaemonSet 调度到不存在的节点上，实现停止功能
    - 新增 Restore 方法，移除 nodeSelector 中的特定节点选择，恢复 DaemonSet 的正常调度- 导入 k8s.io/apimachinery/pkg/types 包，用于处理合并补丁类型

--- kom/ctl_ds.go
+++ kom/ctl_ds.go
@@ -1,9 +1,57 @@
 package kom
 
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/types"
+)
+
 type daemonSet struct {
 	kubectl *Kubectl
 }
 
 func (r *daemonSet) Restart() error {
 	return r.kubectl.Ctl().Rollout().Restart()
+}
+func (r *daemonSet) Stop() error {
+
+	patchData := `{
+  "spec": {
+    "template": {
+      "spec": {
+        "nodeSelector": {
+          "kubernetes.io/hostname": "non-existent-node"
+        }
+      }
+    }
+  }
+}`
+	var item interface{}
+	err := r.kubectl.Patch(&item, types.MergePatchType, patchData).Error
+
+	if err != nil {
+		return fmt.Errorf("stop %s/%s error %v", r.kubectl.Statement.Namespace, r.kubectl.Statement.Name, err)
+	}
+	return nil
+}
+func (r *daemonSet) Restore() error {
+
+	patchData := `{
+  "spec": {
+    "template": {
+      "spec": {
+        "nodeSelector": {
+          "kubernetes.io/hostname": null
+        }
+      }
+    }
+  }
+}`
+	var item interface{}
+	err := r.kubectl.Patch(&item, types.MergePatchType, patchData).Error
+
+	if err != nil {
+		return fmt.Errorf("restore %s/%s error %v", r.kubectl.Statement.Namespace, r.kubectl.Statement.Name, err)
+	}
+	return nil
 }
