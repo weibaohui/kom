ac4e857a1f95743e77f08a0616abf302585181ce
weibaohui <weibaohui@yeah.net>
2025-02-21 11:43:00 +0800

    refactor(example): 移除 pod 监视器代码
    - 删除了 InitPodWatcher() 函数，减少了冗余代码
    - 保留了 initCRDWatcher() 函数，继续监控 CRD 资源
    - 简化了 example.go 文件的结构，提高了可读性

--- example/example.go
+++ example/example.go
@@ -179,40 +179,7 @@ func initCRDWatcher() error {
 	}()
 	return err
 }
-func InitPodWatcher() error {
 
-	var watcher watch.Interface
-
-	var pod corev1.Pod
-	err := kom.DefaultCluster().Resource(&pod).
-		Namespace("default").Watch(&watcher).Error
-	if err != nil {
-		fmt.Printf("Create Watcher Error %v", err)
-		return err
-	}
-	go func() {
-		defer watcher.Stop()
-
-		for event := range watcher.ResultChan() {
-			err := kom.DefaultCluster().Tools().ConvertRuntimeObjectToTypedObject(event.Object, &pod)
-			if err != nil {
-				fmt.Printf("无法将对象转换为 *v1.Pod 类型: %v", err)
-				return
-			}
-			// 处理事件
-			switch event.Type {
-			case watch.Added:
-				fmt.Printf("Added Pod [ %s/%s ]\n", pod.Namespace, pod.Name)
-			case watch.Modified:
-				fmt.Printf("Modified Pod [ %s/%s ]\n", pod.Namespace, pod.Name)
-			case watch.Deleted:
-				fmt.Printf("Deleted Pod [ %s/%s ]\n", pod.Namespace, pod.Name)
-			}
-		}
-	}()
-
-	return nil
-}
 func yamlApplyDelete() {
 	yaml := `apiVersion: apisix.apache.org/v2
 kind: ApisixRoute
