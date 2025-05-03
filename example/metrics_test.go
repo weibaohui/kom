ad0141e1773902de291b4e17bd1b3157ed9c09c3
weibaohui <weibaohui@yeah.net>
2025-05-03 18:43:14 +0800

    Update example/metrics_test.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- example/metrics_test.go
+++ example/metrics_test.go
@@ -24,17 +24,26 @@ func TestPrintTopPodList(t *testing.T) {
 }
 func TestPrintTopSinglePod(t *testing.T) {
 
-	result, err := kom.DefaultCluster().
-		Resource(&v1.Pod{}).Namespace("default").
-		Name("cpu-memory-fluctuation-advanced").
-		Ctl().Pod().Top()
+    result, err := kom.DefaultCluster().
+        Resource(&v1.Pod{}).Namespace("default").
+        Name("cpu-memory-fluctuation-advanced").
+        Ctl().Pod().Top()
 
-	if err != nil {
-		t.Log(err.Error())
-	}
+    if err != nil {
+        t.Log(err.Error())
+        t.Fail() // 添加失败处理
+    }
 
-	t.Logf("\n%s\n", utils.ToJSON(result))
-
+    t.Logf("\n%s\n", utils.ToJSON(result))
+    // 添加断言验证结果
+    if err == nil {
+        if len(result) != 1 {
+            t.Errorf("预期结果应该只包含1个Pod，但获得了%d个", len(result))
+        }
+        if result[0].Name != "cpu-memory-fluctuation-advanced" {
+            t.Errorf("预期Pod名称为cpu-memory-fluctuation-advanced，但获得了%s", result[0].Name)
+        }
+    }
 }
 func TestPrintPodMetrics(t *testing.T) {
 
